ee8c2b253ae462054b90d5ffdd97db2e726c139a
oti <otiext@gmail.com>
2020-07-31 13:31:53 +0900

    feat(script): dialog.js で dialog-polyfill を import した

--- src/script/module/dialog.ts
+++ src/script/module/dialog.ts
@@ -1,6 +1,9 @@
+import dialogPolyfill from "dialog-polyfill";
+
 export const dialog = ({ target }) => {
   const selector = `#${target.dataset.dialog}`;
   const dialog = document.querySelector(selector) as HTMLDialogElement;
+  dialogPolyfill.registerDialog(dialog);
   dialog.showModal();
   dialog.querySelector(".close").addEventListener(
     "click",
