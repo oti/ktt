25254176732fc36e2502f12ff99a70149eec6375
oti <otiext@gmail.com>
2020-07-31 09:55:19 +0900

    feat(generator): src/image/photo/ から pug で必要なデータを生成する data.js を追加した

---
+++ generator/data.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const fs = require("fs");
+const input = process.argv[2] || "src/image/photo/";
+const output = process.argv[3] || "src/pug/data.pug";
+
+const files = fs.readdirSync(input).filter((v) => {
+  return /.+\.jpg$/.test(v) && !/^thumb/.test(v);
+});
+
+const toBasename = (value) => {
+  return value.split(".")[0];
+};
+
+// 2020-01-01 → 2020年1月1日
+const toHeadingString = (value) => {
+  return (
+    toBasename(value)
+      .split("-")
+      .slice(0, 2)
+      .map((v) => Number(v))
+      .join("年") + "月"
+  );
+};
+
+const generate_data_pug = () => {
+  // ファイル名から YYYY年MM月 に変換した配列を作る
+  const duplicated_list = files.map((image) => {
+    return toHeadingString(image);
+  });
+  // ユニークな { heading: 'YYYY年MM月'} の配列を作る
+  const data = [...new Set(duplicated_list)].reverse().map((heading) => ({
+    heading: heading,
+    images: [],
+  }));
+
+  files.forEach((image) => {
+    const index = data.findIndex(
+      (items) => items.heading === toHeadingString(image)
+    );
+    data[index].images.unshift(toBasename(image));
+  });
+
+  const [recent1, recent2, ...rest] = data;
+  const content = `- const recent = ${JSON.stringify([
+    recent1,
+    recent2,
+  ])}\n  const rest = ${JSON.stringify(rest)}`;
+
+  fs.writeFileSync(output, content, (err) => {
+    if (err) throw err;
+  });
+};
+
+generate_data_pug();
