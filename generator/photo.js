c950476ad3fd6975585fca205301e4b10922f94a
oti <otiext@gmail.com>
2020-07-31 09:33:52 +0900

    feat(generator): オリジナルデータから掲載最適化版とサムネールを書き出す photo.js を追加した

---
+++ generator/photo.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const sharp = require("sharp");
+
+const inputDir = process.argv[2] || "src/image/photo-original/";
+const outputDir = process.argv[3] || "src/image/photo/";
+
+const images = fs.readdirSync(inputDir).filter((v) => {
+  return /.+\.jpg$/.test(v);
+});
+
+const convert = () => {
+  images.forEach((image) => {
+    // スペースが含まれているファイル名なので決め打ちで作る
+    const fileBasename = image.split(" ")[0];
+
+    sharp(`${inputDir}${image}`)
+      .resize({ width: 1280 })
+      .jpeg({ quality: 70 })
+      .toFile(`${outputDir}${fileBasename}.jpg`)
+      .then(() => {
+        console.log(`${outputDir}${fileBasename}.jpg has been saved!`);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
+};
+
+const convertThumb = () => {
+  images.forEach((image) => {
+    // スペースが含まれているファイル名なので決め打ちで作る
+    const fileBasename = image.split(" ")[0];
+
+    sharp(`${inputDir}${image}`)
+      .resize({ width: 125, height: 125, fit: "cover" })
+      .jpeg({ quality: 50 })
+      .toFile(`${outputDir}thumb_${fileBasename}.jpg`)
+      .then(() => {
+        console.log(`${outputDir}thumb_${fileBasename}.jpg has been saved!`);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
+};
+
+convert();
+convertThumb();
