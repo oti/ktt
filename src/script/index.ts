71294f41ac6153326654fdee08785808043242fb
oti <otiext@gmail.com>
2022-02-27 13:49:13 +0900

    fix: all.pug と JS を削除した

--- src/script/index.ts
+++
@@ -1,6 +0,0 @@
-import { more } from "./module/more";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const $button = document.querySelector(".more") as HTMLButtonElement;
-  $button && $button.addEventListener("click", more, false);
-});
